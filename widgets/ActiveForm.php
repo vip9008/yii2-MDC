66cbe7f15344907b313e6b1523fd5b7f715695ed
Hussain Almoamen <almoamen.m.h@gmail.com>
2018-06-26 14:23:13 +0300

    add required assets for forms

--- widgets/ActiveForm.php
+++ widgets/ActiveForm.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace vip9008\MDC\widgets;
-
 use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm as BaseActiveForm;
+
+use vip9008\MDC\assets\MenuAsset;
+use vip9008\MDC\assets\SelectionControlAsset;
+use vip9008\MDC\assets\TextFieldAsset;
 
 class ActiveForm extends BaseActiveForm
 {
@@ -13,6 +16,10 @@ class ActiveForm extends BaseActiveForm
 
     public function init()
     {
+        MenuAsset::register($this->getView());
+        SelectionControlAsset::register($this->getView());
+        TextFieldAsset::register($this->getView());
+        
         $this->themeColor = ArrayHelper::remove($this->options, 'themeColor', $this->themeColor);
 
         parent::init();
