c06a0092baaef0abd9b39a9d37d593c1471b7b45
Hussain Almoamen <ave9008@gmail.com>
2019-11-25 16:13:17 +0300

    Create PickerAsset.php

---
+++ assets/PickerAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace vip9008\MDC\assets;
+
+use yii\web\AssetBundle;
+
+class PickerAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/vip9008/md-assets';
+    public $css = [
+        "css/mdc.picker.css",
+    ];
+    public $js = [
+        "js/mdc.picker.js",
+    ];
+    public $depends = [
+        'vip9008\MDC\assets\MaterialGrid',
+    ];
+}
