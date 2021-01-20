a1c60722a060ffabf64006030c3f44750625bfb4
Murtadha Almoamen <murtadha.isa@scriptbh.com>
2021-01-20 16:54:49 +0300

    update

--- assets/DatePickerAsset.php
+++ assets/DatePickerAsset.php
@@ -4,14 +4,14 @@ namespace vip9008\MDC\assets;
 
 use yii\web\AssetBundle;
 
-class PickerAsset extends AssetBundle
+class DatePickerAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/vip9008/md-assets';
     public $css = [
-        "css/mdc.picker.css",
+        "css/mdc.date.picker.css",
     ];
     public $js = [
-        "js/mdc.picker.js",
+        "js/mdc.date.picker.js",
     ];
     public $depends = [
         'vip9008\MDC\assets\MaterialGrid',
